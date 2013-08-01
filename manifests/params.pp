a5e96dbd8e94205e3a52dc16e5274979e06d64f5
Alessandro Franceschi <al@lab42.it>
2013-08-01 13:18:15 +0200

    Example42-tools/module_clone.sh -m nova -n ceph

---
+++ manifests/params.pp
@@ -0,0 +1,111 @@
+# Class: ceph::params
+#
+# This class defines default parameters used by the main module class ceph
+# Operating Systems differences in names and paths are addressed here
+#
+# == Variables
+#
+# Refer to ceph class for the variables defined here.
+#
+# == Usage
+#
+# This class is not intended to be used directly.
+# It may be imported or inherited by other classes
+#
+class ceph::params {
+
+  ### Application related parameters
+
+  $package = $::operatingsystem ? {
+    /(?i:Debian|Ubuntu|Mint)/ => 'ceph-compute',
+    default                   => 'openstack-ceph-compute',
+  }
+
+  $service = $::operatingsystem ? {
+    /(?i:Debian|Ubuntu|Mint)/ => 'ceph-compute',
+    default                   => 'openstack-ceph-compute',
+  }
+
+  $service_status = $::operatingsystem ? {
+    default => true,
+  }
+
+  $process = $::operatingsystem ? {
+    default => 'ceph',
+  }
+
+  $process_args = $::operatingsystem ? {
+    default => '',
+  }
+
+  $process_user = $::operatingsystem ? {
+    default => 'ceph',
+  }
+
+  $config_dir = $::operatingsystem ? {
+    default => '/etc/ceph',
+  }
+
+  $config_file = $::operatingsystem ? {
+    default => '/etc/ceph/ceph.conf',
+  }
+
+  $config_file_mode = $::operatingsystem ? {
+    default => '0644',
+  }
+
+  $config_file_owner = $::operatingsystem ? {
+    default => 'root',
+  }
+
+  $config_file_group = $::operatingsystem ? {
+    default => 'root',
+  }
+
+  $pid_file = $::operatingsystem ? {
+    default => '/var/run/ceph.pid',
+  }
+
+  $data_dir = $::operatingsystem ? {
+    default => '/etc/ceph',
+  }
+
+  $log_dir = $::operatingsystem ? {
+    default => '/var/log/ceph',
+  }
+
+  $log_file = $::operatingsystem ? {
+    default => [ '/var/log/ceph/ceph-compute.log' , '/var/log/ceph/ceph-manage.log' ],
+  }
+
+  $port = '5000'
+  $protocol = 'tcp'
+
+  # General Settings
+  $my_class = ''
+  $source = ''
+  $source_dir = ''
+  $source_dir_purge = false
+  $template = ''
+  $options = ''
+  $service_autorestart = true
+  $version = 'present'
+  $absent = false
+  $disable = false
+  $disableboot = false
+
+  ### General module variables that can have a site or per module default
+  $monitor = false
+  $monitor_tool = ''
+  $monitor_target = $::ipaddress
+  $firewall = false
+  $firewall_tool = ''
+  $firewall_src = '0.0.0.0/0'
+  $firewall_dst = $::ipaddress
+  $puppi = false
+  $puppi_helper = 'standard'
+  $debug = false
+  $audit_only = false
+  $noops = false
+
+}
---
+++ manifests/puppi.pp
@@ -0,0 +1,33 @@
+class ceph::puppi {
+
+  # For Puppi 2 (WIP)
+  $classvars=get_class_args()
+  puppi::ze { 'ceph':
+    ensure    => $ceph::manage_file,
+    variables => $classvars,
+    helper    => $ceph::puppi_helper,
+    noop      => $ceph::bool_noops,
+  }
+
+  # For Puppi 1
+  puppi::info::module { "ceph":
+    packagename => "${ceph::package}",
+    servicename => "${ceph::service}",
+    processname => "${ceph::process}",
+    configfile  => "${ceph::config_file}",
+    configdir   => "${ceph::config_dir}",
+    pidfile     => "${ceph::pid_file}",
+    datadir     => "${ceph::data_dir}",
+    logdir      => "${ceph::log_dir}",
+    protocol    => "${ceph::protocol}",
+    port        => "${ceph::port}",
+    description => "What Puppet knows about ceph" ,
+    # run         => "ceph -V###",
+  }
+
+  puppi::log { "ceph":
+    description => "Logs of ceph" ,  
+    log      => "${ceph::log_file}",
+  }
+
+}
